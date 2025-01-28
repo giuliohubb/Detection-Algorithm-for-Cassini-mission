85ce358d9c5acce9e0c025dbc4eb294275a03701
giuliohubb <53786778+giuliohubb@users.noreply.github.com>
2025-01-28 15:57:33 +0100

    Update inferred_all.py

--- inferred_all.py
+++ inferred_all.py
@@ -25,66 +25,25 @@ import yolo_inference_lib as yil
 import json
 import pickle
 
-images = yil.load_images('/lre/home/gquaglia/cassini_full/*.IMG', 32)
-yil.exposure_duration('/lre/home/gquaglia/cassini_full', images)
-images8 = yil.filter_8bit_images(images)
-distance_qmpf, time, latitude, xyz_IAU, distance_enceladus, xyz_ence =yil.distance('/lre/home/gquaglia/cassini_full', 'Enceladus', images8)
-images8_dict, images8_list = yil.image_division(images8)
-yolo_results = yil.upload_inferred_results('/lre/home/gquaglia/env/yolov5/runs/detect/exp9_cassini_full/labels')
+path_images = 'path/to/your/images/'
+path_yolo = 'path/to/labels/from/yolov5/inference'
 
 
-# Specifica il percorso e il nome del file
-file_path = '/lre/home/gquaglia/background_cassinifull/dic_mean.json'
+images = yil.load_images(path + '*.IMG', 32) #import the images
+yil.exposure_duration(path, images) #select only the ones with an exposure duratio <= 2s
+images8 = yil.filter_8bit_images(images) #select only the 8bit images
+yolo_results = yil.upload_inferred_results(path_yolo) # import the results obtained after inferring yolo on the images
 
-# Carica il dizionario dal file JSON
-with open(file_path, 'r') as json_file:
-    mean_dict = json.load(json_file)
+#this is the path where you stored the thresholds from the 'calcolo_backmean.py' code
 
-file_path = '/lre/home/gquaglia/background_cassinifull/dic_std.json'
+file_path = '/lre/home/gquaglia/background_cassinifull/dic_thresh.json' 
 
-# Carica il dizionario dal file JSON
-with open(file_path, 'r') as json_file:
-    std_dict = json.load(json_file)
-
-# Specifica il percorso e il nome del file
-file_path = '/lre/home/gquaglia/background_cassinifull/dic_thresh.json'
-
-# Carica il dizionario dal file JSON
 with open(file_path, 'r') as json_file:
     thresh = json.load(json_file)
 
 rp_final, stars, sat, gain_dic, energy = yil.inference_to_regionprops(yolo_results, images8_list, thresh)
-df_final, df_dist, df_time, df_lat = yil.build_dataframe(xyz_IAU, rp_final, latitude, distance_qmpf, time, images8_dict, thresh, energy,distance_enceladus, images8_list, thresh)
-#df_final.to_pickle('/lre/home/gquaglia/background_cassinifull/dataframe_cassinifull1')
-#import joblib
-#joblib.dump(rp_final, '/lre/home/gquaglia/background_cassinifull/cosmics2.pkl')
-#with open('/lre/home/gquaglia/background_cassinifull/energy_dictionary_backmedian.pkl', 'wb') as f:
-   #pickle.dump(energy, f)
-#keys = list(rp_final.keys())
-#half = len(keys) // 2
-regionprops1= defaultdict()
-for i in rp_final:
-    for j in range(len(rp_final[i])):
-        regionprops1[i]= [rp_final[i][j][0].centroid, rp_final[i][j][0].area, rp_final[i][j][0].eccentricity, rp_final[i][j][0].orientation,  rp_final[i][j][0].axis_major_length, rp_final[i][j][0].axis_minor_length]
-        
-# First half of the dictionary
-#data_part1 = {key: rp_final[key] for key in keys[:half]}
-
-# Second half of the dictionary
-#data_part2 = {key: rp_final[key] for key in keys[half:]}
-#with open('/lre/home/gquaglia/background_cassinifull/cosmics4.pkl', 'wb') as f:
- #   pickle.dump(data_part1, f)
-with open('/lre/home/gquaglia/background_cassinifull/cosmics_5RS.pkl', 'wb') as f:
-    pickle.dump(regionprops1, f)
-
-#with open('/lre/home/gquaglia/background_cassinifull/cosmics1.pkl', 'wb') as f:
-    #pickle.dump(rp_final, f)
-#
-#with open('/lre/home/gquaglia/background_cassinifull/stars1.pkl', 'wb') as f:
-   # pickle.dump(stars, f)
-
-#with open('/lre/home/gquaglia/background_cassinifull/satellites1.pkl', 'wb') as f:
- #   pickle.dump(sat, f)
-        
-#with open('saved_dictionary.pkl', 'rb') as f:
-    #loaded_dict = pickle.load(f)
+# rp_final = dictionary containing for every image the regionprops objects (from scipy) in which there are many information about the cosmic rays in your images (area, centroids, orientation, eccentricity etc.)
+# stars = same as for rp_final but for stars
+# sat = same as for rp_final and stars but for satellites
+# gain_dic = dictionary with the camera gain used for the images
+# energy = dictionary with all the energies calculated for the cosmic rays
